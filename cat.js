7cd6dae330bf82152d1b814243bf2610b6ef3c9c
kingsley996 <linhdang.dev96@gmail.com>
2018-08-07 16:35:27 +0700

    instance demo

--- cat.js
+++ cat.js
@@ -1,8 +1,18 @@
-function Cat() {
+const Mouse = require('./mouse');
+const mickey = new Mouse("jerry");
+const tom = new Cat('tom');
+
+
+function Cat(name) {
+    this.name = name;
     this.stomach = [];
 }
 
 Cat.prototype.eat = function(mouse) {
     this.stomach.push(mouse);
+    mouse.die();
 }
+tom.eat(mickey);
+console.log(tom);
+console.log(mickey);
 module.exports = Cat;   
